56c1d29b2012f431438d0f2710cd03b05bc8a297
VikoPiko <vikoasmatiko@gmail.com>
2024-01-14 14:00:33 +0200

    Add project files.

---
+++ glfwTest/src/Application.cpp
@@ -0,0 +1,117 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include "Renderer.h"
+
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "Shader.h"
+#include "Texture.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+#include "tests/TestClearColor.h"
+#include "tests/TestTexture2D.h"
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(960, 540, "OPEN_GL", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glfwSwapInterval(1);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "SOMETHING NOT OKAY BOSS\n";
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+    {
+      
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+        
+        Renderer renderer;
+
+        ImGui::CreateContext();
+        ImGui::StyleColorsDark();
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init("#version 130");
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+     
+        test::Test* currentTest = nullptr;
+        test::TestMenu* TestMenu = new test::TestMenu(currentTest);
+        currentTest = TestMenu;
+
+        TestMenu->RegisterTest<test::TestClearColor>("Clear Color");
+        TestMenu->RegisterTest<test::TestTexture2D>("Textures");
+
+        while (!glfwWindowShouldClose(window))
+        {
+            GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
+            renderer.Clear();
+
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            if (currentTest)
+            {
+                currentTest->OnUpdate(0.0f);
+                currentTest->OnRender();
+                ImGui::Begin("Test");
+                if (currentTest != TestMenu && ImGui::Button("<-"))
+                {
+                    delete currentTest;
+                    currentTest = TestMenu;
+                }
+                currentTest->OnImGuiRender();
+                ImGui::End();
+            }
+      
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+
+        delete currentTest; // optimize(check logic).
+        if (currentTest != TestMenu)
+            delete TestMenu; 
+
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
